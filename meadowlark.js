b7ccc5cce2f96f3bfa86314f83d03f4413b979f7
Mikkel Simonov <mikkel27@mail.ru>
2022-06-09 19:07:56 +0300

    add get put and delete API examples

--- meadowlark.js
+++ meadowlark.js
@@ -99,7 +99,48 @@ app.post('/process-contact-2', (req, res) => {
   }
 })
 
-/**********/
+/***** ПРИМЕР АПИ *****/
+
+let tours = [
+  { id: 0, name: 'Худ-Ривер', price: 99.99 },
+  { id: 1, name: 'Орегон Коуст', price: 149.95 },
+]
+
+app.get('/api/tours', (req, res) => {
+  
+  const toursXml = '<?xml version="1.0"?><tours>' +
+    tours.map(p => 
+      `<tour price="${p.price}"> id="${p.id}">${p.name}</tour>`
+    ).join('') + '</tours>'
+
+  const toursText = tours.map(p => 
+      `${p.id}: ${p.name} ${p.price}`
+    ).join('\n')
+
+  res.format({
+    'application/json': () => res.json(tours),
+    'application/xml': () => res.type('application/xml').send(toursXml),
+    'text/xml': () => res.type('text/xml').send(toursXml),
+    'text/plain': () => res.type('text/plain').send(toursText),
+  })
+})
+
+/***** ПРИМЕР АПИ PUT, DELETE *****/
+
+app.put('/api/tour/:id', (req, res) => {
+  const p = tours.find(p => p.id === parseInt(req.params.id))
+  if (!p) return res.status(404).json({ error: 'No such tour exists' })
+  if (req.body.name) p.name = req.body.name
+  if (req.body.price) p.price = req.body.price
+  res.json({ success: true })
+})
+
+app.delete('/api/tour/:id', (req, res) => {
+  const idx = tours.findIndex(tour => tour.id === parseInt(req.params.id))
+  if (idx < 0) return res.json({ error: 'Такого тура не существует' })
+  tours.splice(idx, 1)
+  res.json({ success: true })
+})
 
 // пользовательская страница 404
 app.use(handlers.notFound)
